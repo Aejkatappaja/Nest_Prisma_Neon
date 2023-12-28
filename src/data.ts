2066648c5f6d21ccce3e43a6b3f2ad2da29e5ddb
Aejkatappaja <joverjlj@gmail.com>
2023-12-28 21:05:52 +0100

    add(custome_data): part without db

---
+++ src/data.ts
@@ -0,0 +1,32 @@
+export const user_data = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    email: 'Sincere@april.biz',
+    role: 'CUSTOMER',
+  },
+  {
+    id: 2,
+    name: 'Ervin Howell',
+    email: 'Shanna@melissa.tv',
+    role: 'CUSTOMER',
+  },
+  {
+    id: 3,
+    name: 'Clementine Bauch',
+    email: 'Nathan@yesenia.net',
+    role: 'ADMIN',
+  },
+  {
+    id: 4,
+    name: 'Patricia Lebsack',
+    email: 'Julianne.OConner@kory.org',
+    role: 'ADMIN',
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    email: 'Lucio_Hettinger@annie.ca',
+    role: 'CUSTOMER',
+  },
+];
