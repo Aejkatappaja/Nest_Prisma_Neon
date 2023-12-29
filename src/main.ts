dcc8a8fa787e39bc3b84d62e58963078cc64d0db
Aejkatappaja <joverjlj@gmail.com>
2023-12-29 20:31:37 +0100

    add(Logger): eg log whole app in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { MyLoggerService } from './my-logger/my-logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //   ,
+  //   {
+  //   logger: ['verbose'],
+  //   bufferLogs: true,
+  // });
+  // app.useLogger(app.get(MyLoggerService));
   await app.listen(3000);
   console.log('server is Running on port 3000');
 }
