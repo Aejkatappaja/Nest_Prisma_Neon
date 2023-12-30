d5868cbb3eff39dd08e8aa25b709d4e16121ff8a
Aejkatappaja <joverjlj@gmail.com>
2023-12-30 21:52:29 +0100

    add(chat): handleConnection & handleDisconnect

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,3 +1,4 @@
+import { Logger } from '@nestjs/common';
 import {
   WebSocketGateway,
   WebSocketServer,
@@ -9,14 +10,29 @@ import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway()
 export class ChatGateway {
+  private readonly logger = new Logger(ChatGateway.name);
+
   @WebSocketServer()
   server: Server;
+
+  afterInit() {
+    this.logger.log('Initialized');
+  }
+
+  handleConnection(client: any) {
+    this.logger.log(`Client id: ${client.id} connected`);
+  }
+
+  handleDisconnect(client: any) {
+    this.logger.log(`Cliend id:${client.id} disconnected`);
+  }
 
   @SubscribeMessage('ping')
   handleMessage(
     @ConnectedSocket() client: Socket,
     @MessageBody() message: string,
   ): void {
+    this.logger.log(`Message received from client id: ${client.id}`);
     // Business logic to save the message to the database
     // Broadcasting the new message to all connected clients, excluding the sender
     client.broadcast.emit('ping', message);
